4c0bf7f52eb9397baed79e501fcdff93f53dc1e9
Feature Creature <jdema003@gold.ac.uk>
2018-08-23 02:34:25 +0100

    removes tag sidebar

--- artist-list.js
+++ artist-list.js
@@ -2,11 +2,9 @@ $(document).ready(function(){
 
     $.getJSON("names.json", function(data){
         var artists = [];
-        // var tags = [];
 
         $.each(data, function(key, val){
             artists.push("<p id='art_"+val.id+"' class='artist'>"+val.name+"</p>");
-            // console.log(val.tag.length)
         });
 
         $("<div/>", {
@@ -14,14 +12,15 @@ $(document).ready(function(){
         }).appendTo(".artist-list");
 
         $(".category,.artist,.tag").click(function(){
-            
+
             var string_id = $(this).attr("id"); //art_ID_SPECULATIVE
             var panel_id = string_id.substr(7); //SPECULATIVE
-            
+
             // reset all lines
             $("#connections-map line").css({"stroke": "gray","opacity":"1","stroke-width": 1})
             // reset all circles
-            $("circle").css({'fill':'white',"stroke":'#ccc'});
+            $("#connections-map circle").css({'fill':'white',"stroke":'#ccc'});
+            $("g.artist circle").css({"display":"none"});
             // reset all listed artists
             $(".artist-list p").css({"text-decoration":"none"});
             // reset all artists on the physical map
@@ -32,8 +31,7 @@ $(document).ready(function(){
             $("#category-list p").css({"text-decoration":"none"});
             // hide all category modals
             $(".category-info").css("display","none");
-            // hide all tag modals
-            $("#tag-list p").css({"text-decoration":"none"});
+
 
 
             // highlight associated entities
@@ -43,19 +41,13 @@ $(document).ready(function(){
                     $("circle#art_"+dataset.edges[i].source.id).css({'fill':'darkgray','stroke':'black'});
                     if(dataset.edges[i].target.type == "artist"){
                         $("g.artist#art_"+dataset.edges[i].target.id).css({"text-decoration":"underline"});
+                        $("g.artist#art_"+dataset.edges[i].target.id+" circle").css({"display":"block"});
                         $(".artist-list#art_"+dataset.edges[i].target.id).css({"text-decoration":"underline"});
                     }
                     if(dataset.edges[i].source.type == "artist"){
                         $("g.artist#art_"+dataset.edges[i].source.id).css({"text-decoration":"underline"});
+                        $("g.artist#art_"+dataset.edges[i].source.id+" circle").css({"display":"block"});
                         $(".artist-list #art_"+dataset.edges[i].source.id).css({"text-decoration":"underline"});
-                    }
-                    if(dataset.edges[i].target.type == "tag"){
-                        $("#tag-list #art_"+dataset.edges[i].target.id).css({"text-decoration":"underline"});
-
-                    }
-                    if(dataset.edges[i].source.type == "tag"){
-                        $("#tag-list #art_"+dataset.edges[i].source.id).css({"text-decoration":"underline"});
-
                     }
                     if(dataset.edges[i].target.type == "category"){
                         $("#category-list #art_"+dataset.edges[i].target.id).css({"text-decoration":"underline"});
@@ -65,7 +57,7 @@ $(document).ready(function(){
                         $("#category-list #art_"+dataset.edges[i].source.id).css({"text-decoration":"underline"});
 
                     }
-                } 
+                }
             }
 
 
@@ -84,15 +76,16 @@ $(document).ready(function(){
                 $(".artist-list #"+string_id).css({"text-decoration":"underline"});
                 // highlight selected artist on the physical map
                 $("g.artist#"+string_id).css({"text-decoration":"underline"});
+                $("g.artist#"+string_id+" circle").css({"display":"block"});
             }
-            
+
             // selected category
             if($(this).hasClass("category")){
                 // show selected category's modal
                 $("#"+"ID_"+panel_id).css({"display":"block", "position":"fixed","right":"18%", "margin-right":"0", "top":"10%"});
                 // highlight this category on the list
                 $("#category-list #"+string_id).css({"text-decoration":"underline"});
-            }       
+            }
 
             // selected tag
             if($(this).hasClass("tag")){
@@ -103,16 +96,7 @@ $(document).ready(function(){
                 $("g.artist#art_ID_" + tagArtist).css({"text-decoration":"underline"})
                 // show selected artist's modal
                 $("#"+"ID_"+tagArtist).css({"display":"block", "position":"fixed","left":"18%", "margin-left":"0", "top":"10%"});
-                // highlight this tag on the list
-                $("#tag-list #"+string_id).css({"text-decoration":"underline"});
-                // highlight all the tags for this artist
             }
-
-
-
         });
-
-
     });
-
 });
